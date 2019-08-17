6bc6b6effcda108d07d200ac64e86e4edb9ac9b6
Daniel Lopez <de.lopezbarron@gmail.com>
2019-08-17 18:17:30 -0500

    Project tested, it works like a charm

--- ramcloud-configure.sh
+++ ramcloud-configure.sh
@@ -4,13 +4,23 @@ MACHINES="$1"
 USER_NAME="$2"
 PRIVATE_KEY="$3"
 
-echo ">> CONFIGURING RAMCLOUD WITH MACHINES: $MACHINES"
+experiment=$(basename $MACHINES)
+scripts=(\
+  "set-ramcloud-dependencies" \
+  "set-ramcloud-compile" \
+  "set-ramcloud-configure" \
+  "set-ramcloud-install" \
+  "set-zookeeper-service")
 
-./set-ramcloud-dependencies.sh $MACHINES $USER_NAME $PRIVATE_KEY
-./set-ramcloud-compile.sh $MACHINES $USER_NAME $PRIVATE_KEY
-./set-ramcloud-configure.sh $MACHINES $USER_NAME $PRIVATE_KEY
-./set-ramcloud-install.sh $MACHINES $USER_NAME $PRIVATE_KEY
-./set-zookeeper-service.sh $MACHINES $USER_NAME $PRIVATE_KEY
+install_id=$(head -1 "${MACHINES}-INSTALL_ID.txt")
+echo ">> WAIT FOR IT YOUNG BLOOD 👽 ID: $install_id"
+for script in "${scripts[@]}"
+do
+  log_file="LOG-"$script"-"$experiment".log"
+  cmd="./$script.sh $MACHINES $USER_NAME $PRIVATE_KEY &> $log_file"
+  eval "$cmd"
+  echo ">> FINISHED $script.sh LOG $log_file 🕶"
+done
 
-echo -e ">> SETUP FINISHED 🌮"
+echo ">> WORK IS DONE 🥃"
 exit 0
